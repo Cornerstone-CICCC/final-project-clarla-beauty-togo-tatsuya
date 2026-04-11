f72005b87c0deeff48102de7faaa15ad0ec0d885
yk-graph <yokokura.tatsuya@gmail.com>
2026-04-10 17:32:46 -0700

    add: tips page section

--- dist/js/modules/home-form.js
+++ dist/js/modules/home-form.js
@@ -3,8 +3,8 @@ export function homeForm() {
   const homeTalkForm = document.getElementById('home-contact-form')
   if (!homeTalkForm) return
 
-  const tabs = homeTalkForm.querySelectorAll('.contact__tab')
-  const panels = homeTalkForm.querySelectorAll('.contact__panel')
+  const tabs = homeTalkForm.querySelectorAll('.home-contact__tab')
+  const panels = homeTalkForm.querySelectorAll('.home-contact__panel')
 
   tabs.forEach((tab) => {
     tab.addEventListener('click', () => {
@@ -12,7 +12,7 @@ export function homeForm() {
 
       tabs.forEach((t) => {
         const isActive = t === tab
-        t.classList.toggle('contact__tab--active', isActive)
+        t.classList.toggle('home-contact__tab--active', isActive)
       })
 
       panels.forEach((panel) => {
